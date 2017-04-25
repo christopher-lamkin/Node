599c6cd6d9f0db9811d9788b9c760aeb78f5f2df
christopher-lamkin <cjlamkin64@gmail.com>
2017-04-25 15:11:52 -0700

    silo API call in separate file within a callback function

--- async/example-callback.js
+++ async/example-callback.js
@@ -1,15 +1,4 @@
-var request = require('request');
-var encodedLocation = 'Philadelphia';
-var url = 'http://api.openweathermap.org/data/2.5/weather?appid=f2c20dc0a7b03b6941785435b7532b7c&q=' + encodedLocation + '&units=imperial';
-
-request({
-  url: url,
-  json: true
-}, function(error, response, body){
-  if (error) {
-    console.log('Unable to fetch weather');
-  } else {
-    // console.log(JSON.stringify(body, null, 4))
-    console.log("It is " + body.main.temp + " degrees in " + body.name + "!");
-  }
+var weather = require('./weather.js');
+weather(function (currentWeather){
+  console.log(currentWeather);
 });
