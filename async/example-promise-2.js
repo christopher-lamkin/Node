69a626e899b9468dcf10273ce189b042abf0ce57
christopher-lamkin <cjlamkin64@gmail.com>
2017-04-26 10:17:49 -0700

    clean up comment section

--- async/example-promise-2.js
+++ async/example-promise-2.js
@@ -1,24 +1,3 @@
-// function doWork(shouldFail){
-//   return new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//       if (typeof shouldFail === 'boolean' && shouldFail === true){
-//         reject('error message');
-//       } else {
-//         resolve('success');
-//       }
-//     }, 2000);
-//   });
-// }
-//
-// doWork().then(function(message){
-//   console.log(message);
-//   return doWork(true);
-// }).then(function(message){
-//   console.log(message);
-// }).catch(function(error){
-//   console.log(error);
-// })
-
 function getLocation(){
   return new Promise(function(resolve, reject){
     resolve('Oakland');
